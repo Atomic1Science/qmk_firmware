9dbe20f1229819135e297623e0adc64670127578
Atomic1Science <atomic1science@gmail.com>
2020-07-23 06:31:35 -0400

    changed config and rules to fit the keeb setup

--- keyboards/handwired/atomic_cake/rules.mk
+++ keyboards/handwired/atomic_cake/rules.mk
@@ -14,6 +14,16 @@ MCU = atmega32u4
 #     software delays.
 F_CPU = 16000000
 
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = caterina
+#
 #
 # LUFA specific
 #
